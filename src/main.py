61fd6d9c878daeb0624b98d97d88f8d7136f23b3
poyrazn <poyrazf@mef.edu.tr>
2020-02-29 01:20:33 +0300

    added druid batch ingestion

--- src/main.py
+++ src/main.py
@@ -1,61 +1,48 @@
-from pyspark.sql.context import SQLContext
-
 from pyspark.sql import SparkSession
 from pyspark import SparkFiles
-from pyspark.sql import Catalog
-from pyspark.sql import DataFrameWriter
-from pyspark.sql.types import DecimalType
-
-path = "../data/PaximumSearchData.json"
-
-spark = SparkSession.builder.master("local").appName("Data Visualization").config("spark.executor.memory", "1gb").getOrCreate()
-sc = spark.sparkContext
-sqlContext = SQLContext(sc)
-sc.addFile(path)
-df = spark.read.json(SparkFiles.get("PaximumSearchData.json")).drop('_corrupt_record').dropna(how='all')
+import os
 
 
-df = df \
-	.withColumn('adults', df.Rooms.Adults) \
-	.withColumn('childrenages', df.Rooms.ChildrenAges) \
-	.withColumn('hotelname', df.Destination.Hotel.Name) \
-	.withColumn('city', df.Destination.Destination.Name) \
-	.withColumn('country', df.Destination.Destination.CountryName) \
-	.withColumn('searchtype', df.Destination.Type) \
-	.withColumn('requestredproductcount', df.Destination.RequestedProductCount.cast("integer")) \
-	.withColumnRenamed('Date', 'searchdate') \
-	.withColumnRenamed('CheckIn', 'checkindate') \
-	.withColumnRenamed('CheckOut', 'checkoutdate') \
-	.withColumnRenamed('Period', 'period') \
-	.withColumnRenamed('ResponseTime', 'responsetime') \
-	.withColumnRenamed('Nationality', 'nationality') \
-	.withColumnRenamed('UserId', 'userid') \
-	.withColumnRenamed('Currency', 'currency') \
-	.withColumnRenamed('UserName', 'username') \
-	.withColumnRenamed('HotelCount', 'hotelcount') \
-	.withColumn('accountroot', df.Accounts.Name[0]) \
-	.drop('Destinations', 'Account', 'Accounts', 'Rooms')
-
-#
-print(df.show(5))
-
-#
-# sqlContext.registerDataFrameAsTable(df, 'Data')
-# print(sqlContext.tableNames())
-# print(sqlContext.table('Data').show(5))
+def init(datapath):
+	spark = SparkSession.builder.master("local").appName("Data Visualization").config("spark.executor.memory", "1gb").getOrCreate()
+	sc = spark.sparkContext
+	sc.addFile(datapath)
+	df = spark.read.json(SparkFiles.get("PaximumSearchData.json")).drop('_corrupt_record').dropna(how='all')
+	return df
 
 
-# df.write.format('json').saveAsTable('Data')
-# print(spark.catalog.listColumns('Data'))
-# df2 = sqlContext.table("Data")
-# print(df2.show(5))
+def applyChanges(df):
+	df = df \
+		.withColumn('adults', df.Rooms.Adults) \
+		.withColumn('childrenages', df.Rooms.ChildrenAges) \
+		.withColumn('hotelname', df.Destination.Hotel.Name) \
+		.withColumn('city', df.Destination.Destination.Name) \
+		.withColumn('country', df.Destination.Destination.CountryName) \
+		.withColumn('searchtype', df.Destination.Type) \
+		.withColumn('requestredproductcount', df.Destination.RequestedProductCount.cast("integer")) \
+		.withColumn('checkindate', df.CheckIn['$date']) \
+		.withColumn('checkoutdate', df.CheckOut['$date']) \
+		.withColumn('searchdate', df.Date['$date']) \
+		.withColumn('responsetime', df.ResponseTime['$numberLong']) \
+		.withColumn('accountroot', df.Accounts.Name[0]) \
+		.drop('Destinations', 'Account', 'Accounts', 'Rooms', '_id', 'Destination', 'Date', 'CheckIn', 'CheckOut')
+	return df
 
 
-# from pydruid.db import connect
-# conn = connect(host='localhost', port=8082, path='/druid/v2/sql/', scheme='http')
-# cursor = conn.cursor()
-# query = "SELECT * FROM PaximumTest"
-# cursor.execute(query)
-# data = cursor.fetchall()
+def ingestion(taskpath, filepath, url):
+	command = f"{taskpath} --file {filepath} --url {url}"
+	os.system(command)
 
 
+if __name__ == '__main__':
+	path = "../data/PaximumSearchData.json"
+
+	df = init(path)
+	df = applyChanges(df)
+	df.write.json('../data/paximum')
+
+	indexTaskPath = "/Users/nehir/apache-druid-0.17.0/bin/post-index-task"
+	filepath = "/Users/nehir/Downloads/SanData/data/paximum-index.json"
+	url = "http://localhost:8081"
+
+	ingestion(indexTaskPath, filepath, url)
